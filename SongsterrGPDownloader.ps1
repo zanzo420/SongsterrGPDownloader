8ebc15ac540cbac04353b07581f8d402eb44113f
Zanzo <zp2kshield@gmail.com>
2024-10-16 17:45:09 -0700

    updates

--- SongsterrGPDownloader.ps1
+++ SongsterrGPDownloader.ps1
@@ -522,6 +522,38 @@ function GetSongsterrDownloadURLs($SongIDs_List)
 }
 #endregion
 
+#region SongsterrAI Generate Guitar Pro Tab Functions
+##
+function Generate-SongsterrAITab([string]$title, [string]$artist, [string]$videoId)
+{
+  $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+  $session.Cookies.Add((New-Object System.Net.Cookie("lastSeenTrack", "/a/wsa/jason-aldean-rearview-town-drum-tab-s737929", "/", "www.songsterr.com")))
+  $session.Cookies.Add((New-Object System.Net.Cookie("LastRef", "d3d3LnNvbmdzdGVyci5jb20=", "/", "www.songsterr.com")))
+  $session.Cookies.Add((New-Object System.Net.Cookie("sr_vpt10", "7", "/", "www.songsterr.com")))
+  $session.Cookies.Add((New-Object System.Net.Cookie("SongsterrT", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIyMzIwNjMwIiwiZW1haWwiOiJ6Q2xvbmVIZXJvQGdtYWlsLmNvbSIsIm5hbWUiOiJ6Q2xvbmVIZXJvIiwicGxhbiI6ImZyZWUiLCJzdWJzY3JpcHRpb24iOm51bGwsInBlcm1pc3Npb25zIjpbXSwic3JhX2xpY2Vuc2UiOiJub25lIiwiZ29vZ2xlIjoiMTAxMTQ4MjQxNzg2MDg4MDQ0MTM0IiwiaWF0IjoxNzI3MDE0NDExLCJpZCI6IjIzMjA2MzAifQ.cFBxdTGqU7kpFGuSK_CtoS88NoyTrhWpZiZ6KTxqrfs", "/", "www.songsterr.com")))
+  $session.Cookies.Add((New-Object System.Net.Cookie("hasEditorBonus", "2024-09-22T13:29:45.638Z", "/", "www.songsterr.com")))
+  $response = Invoke-WebRequest -UseBasicParsing -Uri "https://www.songsterr.com/api/song/transcribe-song" `
+  -Method POST `
+  -WebSession $session `
+  -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0" `
+  -Headers @{
+  "Accept" = "*/*"
+    "Accept-Language" = "en-US,en;q=0.5"
+    "Accept-Encoding" = "gzip, deflate, br, zstd"
+    "Referer" = "https://www.songsterr.com/a/wa/submit"
+    "Origin" = "https://www.songsterr.com"
+    "DNT" = "1"
+    "Sec-Fetch-Dest" = "empty"
+    "Sec-Fetch-Mode" = "cors"
+    "Sec-Fetch-Site" = "same-origin"
+    "Priority" = "u=0"
+  } `
+  -ContentType "application/json" `
+  -Body "{`"title`":`"$($title)`",`"artist`":`"$($artist)`",`"videoId`":`"$($videoId)`"}"
+
+  return $response.Content
+}
+#endregion
 
 #region Misc Utility Functions
 ###############################################
---
+++ convWinForm.ps1
@@ -0,0 +1,8 @@
+function Convert-WinForm([string]$Source, [string]$Destination)
+{
+	#$Source = "C:\Users\Zanzo\source\repos\WinForms2Powershell-GUI\Form1.Designer.cs"
+	#$Destination = "D:\GitHub\SongsterrGPDownloader\gui\"
+	Convert-Form -Path $Source -Destination $Destination -Encoding ascii -force
+}
+
+Convert-WinForm -Source "C:\Users\Zanzo\source\repos\WinForms2Powershell-GUI\frmMain.Designer.cs" -Destination "D:\GitHub\WinForms2Powershell-GUI\"
